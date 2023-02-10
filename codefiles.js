9f2778c3cac3da8472a8c1b0c2f039ec2e5a0f21
Tommerty <tommerty@github.com>
2023-02-10 13:24:17 +0000

    adding JS

---
+++ codefiles.js
@@ -0,0 +1,16 @@
+showCTA(`<div id="discord-cta" class="bg-teal-600 p-9 text-center rounded-lg shadow-xl">
+
+<h1 class="text-center text-white items-center text-2xl font-extrabold">Join our Newsletter <i
+        class="fa-solid fa-envelope"></i></h1>
+
+<p class="pt-3 pb-9 text-xl">Keep up to date with our company by signing up to our amazing monthly
+    newsletter.<br><span class="font-bold text-lg">No spam, all amazing</span></p>
+<a href="#"
+    class="text-center text-white bg-teal-700 hover:bg-teal-900 hover:text-white p-2 rounded-lg font-bold text-xl">
+    <i class="fa-solid fa-envelope text-white text-xl"></i> Sign Up</a>
+</div>`);
+
+showSignin(``);
+
+
+
